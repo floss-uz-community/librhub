71d55c6c1654b8847c1078898bfaec9befd39c37
Javoxir G'ulomjonov <javohirtech@gmail.com>
2025-09-27 09:47:34 +0500

    refactor: restructure frontend components and update configurations

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,6 +1,12 @@
-import { useTranslations } from "next-intl";
+import { getPosts } from '@/shared/config/api/testApi';
 
-export default function Home() {
-  const t = useTranslations()
-  return <>{t("title")}</>;
+export default async function Home() {
+  const res = await getPosts({ _limit: 1 });
+  console.log('SSR res', res.data);
+
+  return (
+    <div className='my-container'>
+      salom
+    </div>
+  );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
-import { routing } from "@/i18n/routing";
-import { redirect } from "next/navigation";
+import { routing } from '@/shared/config/i18n/routing';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+// This page only renders when the app is built statically (output: 'export')
+export default function RootPage() {
   redirect(routing.defaultLocale);
 }
